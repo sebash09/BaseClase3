4600d5ded95da407d6b7dc7bc8c8ee49378983ba
sebash09 <66684387+sebash09@users.noreply.github.com>
2024-03-08 10:33:40 -0500

    Update leerdatos.php

--- leerdatos.php
+++ leerdatos.php
@@ -7,12 +7,12 @@ $objectResponse = new stdClass();
 date_default_timezone_set("UTC");
 
 try {
-    $accesoBD = new accesoBD();
-    $objectResponse = $accesoBD->leerDatos();
+    $accesoBD = new accesoBD(); // Crear una instancia de la clase accesoBD
+    $objectResponse = $accesoBD->leerDatos(); // Llamar al método leerDatos()
 } catch (Exception $e) {
-    $objectResponse->code = '500';
-    $objectResponse->message = $e->getMessage();
+    $objectResponse->code = '500'; // Código para error interno del servidor
+    $objectResponse->message = $e->getMessage(); // Agregar el mensaje de error a la respuesta
 }
 
-echo json_encode($objectResponse);
+echo json_encode($objectResponse); // Utilizar json_encode() para convertir el objeto a JSON
 ?>
